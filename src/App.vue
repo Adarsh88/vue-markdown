177b5ac9fc5dc7d5598afff65d6367f6f1ef0276
Adarsh <aaa.narayanan@gmail.com>
2021-09-08 19:39:09 +0530

    added new file

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Markdown />
+</template>
+
+<script>
+import Markdown from "./components/Markdown.vue";
+
+export default {
+  name: "App",
+  components: { Markdown },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  color: #2c3e50;
+}
+</style>
